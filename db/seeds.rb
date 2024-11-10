4a9ea52c4153a9d135f3087c0e994a8f00eedb40
Yeseullyn Lee <LYSL1025@gmail.com>
2024-11-09 18:50:02 -0600

    Corrected column name for FK keys in all tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Create categories
+categories = %w[Clothing Accessories Home_Decor]
+categories.each { |name| Category.create(name: name) }
+
+# Create products with random data
+10.times do
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 10..100),
+    stock_quantity: rand(1..50),
+    category_id: Category.all.sample
+  )
+end
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
