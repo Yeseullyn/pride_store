7cfed980e0ba69e6769f93896f391a413d7befd6
Yeseullyn Lee <LYSL1025@gmail.com>
2024-11-12 10:32:00 -0600

    Added permition for Category and Product in ActiveAdmin.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :price, :stock_quantity, :category_id
+  permit_params :name, :price, :stock_quantity, :category_id
   #
   # or
   #
